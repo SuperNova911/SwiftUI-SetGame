76bcad64a656e627d9a6f6ebc0bd9723f9a0f105
Suwhan Kim <suwhan77@naver.com>
2020-08-08 01:00:25 +0900

    Initial Commit

---
+++ SwiftUI SetGame/SwiftUI_SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SetGameApp.swift
+//  SwiftUI SetGame
+//
+//  Created by 김수환 on 2020/08/08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
